ce9de611f77f57c03eec74d2594f93a0fd376a43
Chet Kener <Cl3Kener@gmail.com>
2014-07-08 19:03:12 -0400

    Fix Freq
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- drivers/cpufreq/cpufreq_hyper.c
+++ drivers/cpufreq/cpufreq_hyper.c
@@ -55,11 +55,11 @@
 #define MAX_FREQUENCY_UP_THRESHOLD              (100)
 #define FREQ_STEP                               (35)
 #define UP_THRESHOLD_AT_MIN_FREQ                (40)
-#define FREQ_FOR_RESPONSIVENESS                 (1008000)
+#define FREQ_FOR_RESPONSIVENESS                 (800000)
 
 #ifdef CONFIG_HAS_EARLYSUSPEND
 #ifdef SUSPEND_FREQ_ON
-#define DEF_SUSPEND_FREQ			(1008000)
+#define DEF_SUSPEND_FREQ			(800000)
 #define FREQ_STEP_SUSPEND                       (40)
 #define SAMPLING_FACTOR_SUSPEND			(3)
 #define DEF_FREQUENCY_UP_THRESHOLD_SUSPEND	(95)
